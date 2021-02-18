4889a0b4219a81a8b7689d80aa8e6a83c7f58675
Francisco de la Vega <fdelavega@ficodes.com>
2021-02-18 17:41:54 +0100

    Update tests

--- test/oauth2-test.js
+++ test/oauth2-test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const util = require('util');
-const FIWAREStrategy = require('../lib/passport-fiware-oauth/oauth2');
+const proxyquire = require('proxyquire')
 
 const CLIENT_ID = 'ABC123';
 const CLIENT_SECRET = 'secret';
@@ -8,26 +8,45 @@ const CLIENT_SECRET = 'secret';
 
 describe('FIWAREStrategy', () => {
 
-    let strategy =  new FIWAREStrategy({
-        clientID: CLIENT_ID,
-        clientSecret: CLIENT_SECRET
-      },function() {});
+    function strategy(fiwareStrategy, config) {
+        if (config == null) {
+            config = {
+                clientID: CLIENT_ID,
+                clientSecret: CLIENT_SECRET
+            };
+        }
+        return new fiwareStrategy(config, function() {});
+    };
+
+    function requireStrategy(requestMock, config) {
+        let FIWAREStrategy = proxyquire('../lib/passport-fiware-oauth/oauth2', {
+            'request': requestMock
+        });
+        return strategy(FIWAREStrategy, config);
+    }
+
+    function requiredRawStrategy(config) {
+        let FIWAREStrategy = require('../lib/passport-fiware-oauth/oauth2');
+        return strategy(FIWAREStrategy, config);
+    }
 
     it('should be named fiware', function () {
+        let strategy = requiredRawStrategy();
         expect(strategy.name).toBe('fiware');
     });
 
     it('should set correct headers', function() {
+        let strategy = requiredRawStrategy();
         let authHeaderValue = 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
         expect(strategy._oauth2._customHeaders['Authorization']).toBe(authHeaderValue);
     });
 
     describe('strategy when providing the IdM host', () => {
-        let strategyH = new FIWAREStrategy({
+        let strategyH = requiredRawStrategy({
             clientID: CLIENT_ID,
             clientSecret: CLIENT_SECRET,
             serverURL: 'http://mycustomidm.com/'
-        }, function() {});
+        });
 
         it('should have changed the default host', function() {
             expect(strategyH.serverURL).toBe('http://mycustomidm.com');
@@ -35,23 +54,18 @@ describe('FIWAREStrategy', () => {
     });
 
     describe('strategy when loading user profile', () => {
-        let strategyP = new FIWAREStrategy({
-            clientID: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            isLegacy: true
-        }, function() {});
 
         it('should load user profile without errors', function(done) {
-            strategyP._oauth2.get = function(url, accessToken, callback) {
+            let strategyP = requireStrategy((params, callback) => {
                 let body = {"organizations": [{ "id": "12", "name": "UPM", "roles": [{"id": "14", "name": "Admin"}] }],
                     "displayName": "Jared Hanson",
                     "app_id": "564",
                     "email": "example@fiware.org",
                     "id": "fiware-user-name",
                     "roles": [{"name": "provider", "id": "106"}]};
-            
-                callback(null, JSON.stringify(body), undefined);
-            };
+
+                callback(null, {}, JSON.stringify(body));
+            });
 
             let callback = (err, profile) => {
                 expect(err).toBeNull();
@@ -76,16 +90,10 @@ describe('FIWAREStrategy', () => {
     });
 
     describe('strategy when loading user profile and encountering an error', () => {
-        let strategyP = new FIWAREStrategy({
-            clientID: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            isLegacy: true
-        }, function() {});
-
         it('should load user profile without errors', function(done) {
-            strategyP._oauth2.get = function(url, accessToken, callback) {
+            let strategyP = requireStrategy((params, callback) => {
                 callback(new Error('something-went-wrong'));
-            };
+            });
 
             let callback = (err, profile) => {
                 expect(err).not.toBeNull();
